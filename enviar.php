9aef8f051e4ccebf8dbce19d6d07349b45c55d3e
PaulaaGr <125744969+PaulaaGr@users.noreply.github.com>
2023-05-25 19:21:00 -0500

    Creación archivos
    Se crearon los archivos php y html, se importó la base de datos

---
+++ enviar.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>estudiante</title>
+</head>
+
+<?php
+    $codigo = $_POST['codigo'];
+    $nombre = $_POST['nombre'];
+    echo "CODIGO <b>" . $codigo . "</b> <br>";
+    echo "NOMBRE <b>" . $nombre . "</b>";
+    ?>
